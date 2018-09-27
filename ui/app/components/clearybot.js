d336dc866d20f787f7a9f9308ef3ac7a473d7933
Hunter Thompson <thompson.grey.hunter@gmail.com>
2018-09-27 10:08:12 -0600

    Removed unused imports. Fixed typo

--- ui/app/components/clearybot.js
+++ ui/app/components/clearybot.js
@@ -1,9 +1,6 @@
 const Component = require('react').Component
 const h = require('react-hyperscript')
 const inherits = require('util').inherits
-const connect = require('react-redux').connect
-const isNode = require('detect-node')
-const findDOMNode = require('react-dom').findDOMNode
 
 module.exports = ClearyComponent
 
